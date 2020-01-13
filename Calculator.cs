2691e88e070a6142bf137b789467cd8821fdaa2e
ChaAarlie <maxime.mare@outlook.com>
2020-01-13 16:54:07 +0100

    commit 6?

--- Calculator.cs
+++ Calculator.cs
@@ -3,7 +3,7 @@ using System;
 namespace githubtest.C__MARE
 {
     public class Calculator
-    {       // teststststtststst
+    {       // ULT TESTs
         static void Main(string[] args)
         {
             // Declare variables and then initialize to zero.
